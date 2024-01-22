bf671b6f9919c6dd7c70b46fbcb7f91c4feb38dc
kimmykokonut@gmail <kimmykokonut@gmail.com>
2024-01-22 12:01:26 -0800

    add ice story form and result
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- MadLibs.Solution/MadLibs/Controllers/HomeController.cs
+++ MadLibs.Solution/MadLibs/Controllers/HomeController.cs
@@ -1,43 +1,68 @@
 using Microsoft.AspNetCore.Mvc;
 using MadLibs.Models;
 
-namespace MadLibs.Controllers 
+namespace MadLibs.Controllers
 {
-  public class HomeController : Controller
-  {
-
-    [Route("/")]
-    public ActionResult Home() { return View(); }
-
-    [Route("/efform")]
-    public ActionResult EFForm() { return View(); }
-    [Route("/efstory")]
-    public ActionResult EFStory(string properNoun, string adj, string color, string animal, string place, string adj1, string mc, string adj2, string mc2, string room, string noun, string noun1, string noun2, string adj3, string nounP, string num, string time, string verb, string adj4, string noun3) //route for form data to go to...
+    public class HomeController : Controller
     {
-        BlankVariable myBlankVariable = new BlankVariable();
 
-        myBlankVariable.ProperNoun = properNoun;
-        myBlankVariable.Adj = adj;
-        myBlankVariable.Color = color;
-        myBlankVariable.Animal = animal;
-        myBlankVariable.Place = place;
-        myBlankVariable.Adj1 = adj1;
-        myBlankVariable.Mc = mc;
-        myBlankVariable.Adj2 = adj2;
-        myBlankVariable.Mc2 = mc2;
-        myBlankVariable.Room = room;
-        myBlankVariable.Noun = noun;
-        myBlankVariable.Noun1 = noun1;
-        myBlankVariable.Noun2 = noun2;
-        myBlankVariable.Adj3 = adj3;
-        myBlankVariable.NounP = nounP;
-        myBlankVariable.Num = num;
-        myBlankVariable.Time = time;
-        myBlankVariable.Verb = verb;
-        myBlankVariable.Adj4 = adj4;
-        myBlankVariable.Noun3 = noun3;
-        
-        return View(myBlankVariable);
+        [Route("/")]
+        public ActionResult Home() { return View(); }
+
+        [Route("/efform")]
+        public ActionResult EFForm() { return View(); }
+
+        [Route("/iceform")]
+        public ActionResult IceForm() { return View(); }
+
+        [Route("/efstory")]
+        public ActionResult EFStory(string properNoun, string adj, string color, string animal, string place, string adj1, string mc, string adj2, string mc2, string room, string noun, string noun1, string noun2, string adj3, string nounP, string num, string time, string verb, string adj4, string noun3) //route for form data to go to...
+        {
+            BlankVariable myBlankVariable = new BlankVariable();
+
+            myBlankVariable.ProperNoun = properNoun;
+            myBlankVariable.Adj = adj;
+            myBlankVariable.Color = color;
+            myBlankVariable.Animal = animal;
+            myBlankVariable.Place = place;
+            myBlankVariable.Adj1 = adj1;
+            myBlankVariable.Mc = mc;
+            myBlankVariable.Adj2 = adj2;
+            myBlankVariable.Mc2 = mc2;
+            myBlankVariable.Room = room;
+            myBlankVariable.Noun = noun;
+            myBlankVariable.Noun1 = noun1;
+            myBlankVariable.Noun2 = noun2;
+            myBlankVariable.Adj3 = adj3;
+            myBlankVariable.NounP = nounP;
+            myBlankVariable.Num = num;
+            myBlankVariable.Time = time;
+            myBlankVariable.Verb = verb;
+            myBlankVariable.Adj4 = adj4;
+            myBlankVariable.Noun3 = noun3;
+
+            return View(myBlankVariable);
+        }
+        [Route("/icestory")]
+        public ActionResult IceStory(string one, string two, string three, string four, string five, string six, string seven, string eight, string nine, string ten, string eleven, string twelve, string thirteen, string fourteen, string fifteen)
+        {
+            IceVariable myIceVariable = new IceVariable();
+            myIceVariable.One = one;
+            myIceVariable.Two = two;
+            myIceVariable.Three = three;
+            myIceVariable.Four = four;
+            myIceVariable.Five = five;
+            myIceVariable.Six = six;
+            myIceVariable.Seven = seven;
+            myIceVariable.Eight = eight;
+            myIceVariable.Nine = nine;
+            myIceVariable.Ten = ten;
+            myIceVariable.Eleven = eleven;
+            myIceVariable.Twelve = twelve;
+            myIceVariable.Thirteen = thirteen;
+            myIceVariable.Fourteen = fourteen;
+            myIceVariable.Fifteen = fifteen;
+            return View(myIceVariable);
+        }
     }
-  }
 }
---
+++ MadLibs.Solution/MadLibs/Models/IceVariable.cs
@@ -0,0 +1,21 @@
+namespace MadLibs.Models
+{
+    public class IceVariable
+    {
+        public string One { get; set; }
+        public string Two { get; set; }
+        public string Three { get; set; }
+        public string Four { get; set; }
+        public string Five { get; set; }
+        public string Six { get; set; }
+        public string Seven { get; set; }
+        public string Eight { get; set; }
+        public string Nine { get; set; }
+        public string Ten { get; set; }
+        public string Eleven { get; set; }
+        public string Twelve { get; set; }
+        public string Thirteen { get; set; }
+        public string Fourteen { get; set; }
+        public string Fifteen { get; set; }
+    }
+}
