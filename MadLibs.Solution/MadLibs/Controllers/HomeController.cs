2125ed56a12dadcd060289c402aeababd6446e82
kimmykokonut@gmail <kimmykokonut@gmail.com>
2024-01-22 10:09:49 -0800

    add mvc functionality
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- MadLibs.Solution/MadLibs/Controllers/HomeController.cs
+++ MadLibs.Solution/MadLibs/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
 
 namespace MadLibs.Controllers 
 {
@@ -11,7 +12,18 @@ namespace MadLibs.Controllers
     [Route("/")]
     public string Story() { return "story here"; }
 
-    // [Route"/form"]
-    // public string Form() { return View(); }
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+    [Route("/story")]
+    public ActionResult Story(string properNoun, string adjective, string color, string animal, string place) //route for form data to go to
+    {
+        BlankVariable myBlankVariable = new BlankVariable();
+        myBlankVariable.ProperNoun = properNoun;
+        myBlankVariable.Adjective = adjective;
+        myBlankVariable.Color = color;
+        myBlankVariable.Animal = animal;
+        myBlankVariable.Place = place;
+        return View(myBlankVariable);
+    }
   }
 }
---
+++ MadLibs.Solution/MadLibs/Models/BlankVariable.cs
@@ -0,0 +1,11 @@
+namespace MadLibs.Models
+{
+    public class BlankVariable
+    {
+        public string ProperNoun { get; set; }
+        public string Adjective { get; set; }
+        public string Color { get; set; }
+        public string Animal { get; set; }
+        public string Place { get; set; }
+    }
+}
