a6b844d4d112842d79625d44d0376fec8817fa1d
kimmykokonut@gmail <kimmykokonut@gmail.com>
2024-01-22 11:09:05 -0800

    add home page link to mad lib story
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- MadLibs.Solution/MadLibs/Controllers/HomeController.cs
+++ MadLibs.Solution/MadLibs/Controllers/HomeController.cs
@@ -5,24 +5,38 @@ namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/hello")]
-
-    public string Hello() { return "Hello Friend!"; }
 
     [Route("/")]
-    public string Story() { return "story here"; }
+    public ActionResult Home() { return View(); }
 
-    [Route("/form")]
-    public ActionResult Form() { return View(); }
-    [Route("/story")]
-    public ActionResult Story(string properNoun, string adjective, string color, string animal, string place) //route for form data to go to
+    [Route("/efform")]
+    public ActionResult EFForm() { return View(); }
+    [Route("/efstory")]
+    public ActionResult EFStory(string properNoun, string adj, string color, string animal, string place, string adj1, string mc, string adj2, string mc2, string room, string noun, string noun1, string noun2, string adj3, string nounP, string num, string time, string verb, string adj4, string noun3) //route for form data to go to...
     {
         BlankVariable myBlankVariable = new BlankVariable();
+
         myBlankVariable.ProperNoun = properNoun;
-        myBlankVariable.Adjective = adjective;
+        myBlankVariable.Adj = adj;
         myBlankVariable.Color = color;
         myBlankVariable.Animal = animal;
         myBlankVariable.Place = place;
+        myBlankVariable.Adj1 = adj1;
+        myBlankVariable.Mc = mc;
+        myBlankVariable.Adj2 = adj2;
+        myBlankVariable.Mc2 = mc2;
+        myBlankVariable.Room = room;
+        myBlankVariable.Noun = noun;
+        myBlankVariable.Noun1 = noun1;
+        myBlankVariable.Noun2 = noun2;
+        myBlankVariable.Adj3 = adj3;
+        myBlankVariable.NounP = nounP;
+        myBlankVariable.Num = num;
+        myBlankVariable.Time = time;
+        myBlankVariable.Verb = verb;
+        myBlankVariable.Adj4 = adj4;
+        myBlankVariable.Noun3 = noun3;
+        
         return View(myBlankVariable);
     }
   }
--- MadLibs.Solution/MadLibs/Models/BlankVariable.cs
+++ MadLibs.Solution/MadLibs/Models/BlankVariable.cs
@@ -3,9 +3,24 @@ namespace MadLibs.Models
     public class BlankVariable
     {
         public string ProperNoun { get; set; }
-        public string Adjective { get; set; }
+        public string Adj { get; set; }
         public string Color { get; set; }
         public string Animal { get; set; }
         public string Place { get; set; }
+        public string Adj1 { get; set; }
+        public string Mc { get; set; }
+        public string Adj2 { get; set; }
+        public string Mc2 { get; set; }
+        public string Room { get; set; }
+        public string Noun { get; set; }
+        public string Noun1 { get; set; }
+        public string Noun2 { get; set; }
+        public string Adj3 { get; set; }
+        public string NounP { get; set; }
+        public string Num { get; set; }
+        public string Time { get; set; }
+        public string Verb { get; set; }
+        public string Adj4 { get; set; }
+        public string Noun3 { get; set; }
     }
 }
